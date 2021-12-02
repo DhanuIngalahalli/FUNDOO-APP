c5ce9139cb83e51c3c6d6a713ad5afef4f176e17
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-12-02 08:18:45 -0800

    Adding:validation to reset page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 //import Registration from './Pages/signup/Registration';
-import Login from './Pages/Login/Login';
+//import Login from './Pages/Login/Login';
 //import Forget from './Pages/Forget/Forget';
-//import Reset from './Pages/Reset/Reset';
+import Reset from './Pages/Reset/Reset';
 
 function App() {
   return (
     <div className="App">
         {/* <Registration /> */}
-        <Login /> 
+       {/* <Login /> */}
          {/*<Forget/> */}
-       {/*  <Reset/> */}
+         <Reset/> 
     </div>
   );
 }
--- src/Pages/Reset/Reset.jsx
+++ src/Pages/Reset/Reset.jsx
@@ -4,6 +4,40 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export class Reset extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            newpassword:"",
+            confirmPwd:"",
+            passworderror:false,
+            confirmPwderror:false,
+        };
+      }
+      validation = () => {
+        let isError = false;
+        const error = this.state;
+        error.newpassworderror = this.state.newpassword === "" ? true : false;
+        error.confirmPwderror = this.state.confirmPwd === "" ? true : false;
+        this.setState({   
+          ...error,
+        });
+        isError = error.newpassword ;
+        isError = error.confirmPwderror;
+        return isError;
+      };
+      next = () => {
+        var isValid = this.validation();
+        if (!isValid) {
+          console.log("successful validation");
+        }
+      };
+      changeHandle =(e)=>{
+          this.setState({  
+          [e.target.name]: e.target.value 
+        })
+    }
+    
     render() {
         return (
             <div class="Reset-password-body">
@@ -11,10 +45,10 @@ export class Reset extends Component {
             <h1 class="Password-header">Reset password</h1>
             <span class="password-text">We'll never share your password with anyone else</span>
             <form>
-                <TextField fullWidth label="new password" id="password" size="medium" margin="dense" sx={{marginTop:'40px'}}/>
-                <TextField fullWidth label="confirm password" id="password" size="medium" margin="dense" sx={{marginTop:'40px'}}/>
+                <TextField fullWidth label="new password" id="password" size="medium" margin="dense"error={this.state.newpassworderror} helperText={this.state.newpassworderror ? "Enter Password" : ''} onChange={e=> this.changeHandle(e)}  sx={{marginTop:'40px'}}/>
+                <TextField fullWidth label="confirm password" id="password" size="medium" margin="dense"error={this.state.confirmPwderror} helperText={this.state.confirmPwderror ? "Re-enter password" : ''} onChange={e=> this.changeHandle(e)} sx={{marginTop:'40px'}}/>
                 <div class="button">
-                        <Button variant="contained"> Next </Button>
+                        <Button variant="contained" onClick={this.next}>Next</Button>
                 </div>
             </form>
         </div>
