7d7992d08a2c0c10660d29066dcd300046b11f06
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-12-02 06:02:07 -0800

    modified:Reset page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,41 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            emailorphone: "",
+            password: "",
+            emailorphoneerror: false,
+            passworderror: false,
+        };
+    }
+
+    validation = () => {
+        let isError = false;
+        const error = this.state;
+        error.emailorphoneerror = this.state.emailorphone === "" ? true : false;
+        error.passworderror = this.state.password === "" ? true : false;
+        this.setState({
+             ...error,
+        });
+       isError = error.emailorphoneerror || error.passworderror;
+       return isError;
+  };
+    next = () => {
+       var isValid = this.validation();
+       if (!isValid) {
+       console.log("successful validation");
+    };
+  }
+    changeHandle =(e)=>{
+    this.setState({
+      [e.target.name]: e.target.value 
+    })
+}
     render() {
+        console.log(this.state)
         return (
             <div class="Login-container">
                 <div class="logo"> Fundoo
@@ -12,16 +46,16 @@ export class Login extends Component {
                 <h1 class="header">Sign in</h1>
                 <span class="text">Use your Fundoo account</span>
                 <formfield>
-                    <TextField fullWidth label="Email or phone" id="emailorPhone" size="medium" margin="normal" sx={{marginTop:'40px'}}/>
+                    <TextField fullWidth label="Email or phone" id="emailorphone" size="medium" margin="normal" error={this.state.emailorphoneerror} helperText={this.state.emailorphoneerror ? "Enter Email or Phone" : ''} onChange={e=> this.changeHandle(e)} sx={{marginTop:'40px'}} />
                     <p class="text1">Forgot email?</p>
-                    <TextField fullWidth label="Enter Password" id="password" size="medium" margin="normal" sx={{marginTop:'40px'}}/>
+                    <TextField fullWidth label="Enter Password" id="password" size="medium" margin="normal" error={this.state.passworderror} helperText={this.state.passworderror ? "Enter Password" : ''} onChange={e=> this.changeHandle(e)} sx={{marginTop:'40px'}}/>
                     <div class="text2">
                         Not your computer? Use Guest mode to sign in privately.
                         <a href="">Learn more</a>
                     </div>
                     <div class="buttons">
                             <Button>Create account</Button>
-                            <Button variant="contained">Next</Button>
+                            <Button variant="contained" onClick={this.next}>Next</Button>
                     </div>
                 </formfield>
                 
